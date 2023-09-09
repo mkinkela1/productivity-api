d59c972a58e7afddc53afbd0e08e229cdc72013b
Matteo Kinkela <mkinkela123@gmail.com>
2023-09-09 20:20:41 +0200

    Dockerize everything, remove boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "src/app.module";
+import "src/config/aliases";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
